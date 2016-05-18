aad5f4ee188d3c5ca10583a7550a9b6852f257fa
Ge Yang <yangge1987@gmail.com>
2016-05-18 16:50:57 -0500

    remove lodash, move luna to peer dependency

--- src/util/isAction.ts
+++ src/util/isAction.ts
@@ -1,7 +1,5 @@
 /** Created by ge on 3/27/16. */
-//import isPlainObject from "lodash/isPlainObject";
-import * as _ from "lodash";
-var isPlainObject = _.isPlainObject;
+var isPlainObject = require("lodash.isplainobject");
 export function isAction(obj:any):boolean {
-    return (typeof obj.type !== "undefined" && _.isPlainObject(obj) && !obj.__isNotAction)
+    return (typeof obj.type !== "undefined" && isPlainObject(obj) && !obj.__isNotAction)
 }
