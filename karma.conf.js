739bc3333db72be6fa1394917041a6ac644eb156
Ge Yang <yangge1987@gmail.com>
2016-12-24 20:33:55 -0600

    remove es6-shim use core-js instead
    not sure how core-js gets passed in though

--- karma.conf.js
+++ karma.conf.js
@@ -24,7 +24,7 @@ module.exports = function (config) {
         // possible values: config.LOG_DISABLE || config.LOG_ERROR || config.LOG_WARN || config.LOG_INFO || config.LOG_DEBUG
         logLevel: config.LOG_INFO,
         autoWatch: true,
-        browsers: ['Chrome'],
+        browsers: ['Safari'],
         singleRun: false,
         // Concurrency level
         // how many browser should be started simultanous
